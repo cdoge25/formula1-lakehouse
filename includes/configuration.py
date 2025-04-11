1705d2f0a2e70a7bda92afd93657a012ce87ffe7
cdoge25 <cdoge25>
2025-04-11 08:17:27 +0000

    ingest and transform

---
+++ includes/configuration.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/haiformula1dl/raw"
+processed_folder_path = "/mnt/haiformula1dl/processed"
+presentation_folder_path = "/mnt/haiformula1dl/presentation"
+
+# COMMAND ----------
+
+#raw_folder_path = 'abfss://raw@haiformula1dl.dfs.core.windows.net'
+#processed_folder_path = 'abfss://processed@haiformula1dl.dfs.core.windows.net'
+#presentation_folder_path = 'abfss://presentation@haiformula1dl.dfs.core.windows.net'
